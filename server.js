4c103640776616906b7ddf71cea8e932d4ddd6a1
tomboy40 <68487885+tomboy40@users.noreply.github.com>
2025-03-06 00:17:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 var port = parseInt(process.env['APP_PORT'] || '8082');
 var appName = process.env['APP_NAME'] || 'myapp';
 
+console.log('APP_DATA:', process.env['APP_NAME'])
+console.log('TEMP:', process.env['TEMP'])
+
+
 var express = require('express');
 var app = express();
 
