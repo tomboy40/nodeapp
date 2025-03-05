347d967ef7c2bc38c7a785dce45eeb92822609b3
tomboy40 <68487885+tomboy40@users.noreply.github.com>
2025-03-06 00:19:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var port = parseInt(process.env['APP_PORT'] || '8082');
 var appName = process.env['APP_NAME'] || 'myapp';
 
-console.log('APP_DATA:', process.env['APP_NAME'])
+console.log('APP_DATA:', process.env['APP_DATA'])
 console.log('TEMP:', process.env['TEMP'])
 
 
