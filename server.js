eb292ed74cabada3e490863fd53a0cdbef46276d
tomboy40 <68487885+tomboy40@users.noreply.github.com>
2025-03-06 00:05:29 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+
+var port = 8082;
+var appName = process.env['APP_NAME'] || 'myapp';
+
+var express = require('express');
+var app = express();
+
+app.get('/' + appName, function(req, res) {
+  res.send('Hi from ' + appName + "!\n');
+});
+
+app.listen(port, 'localhost', function() {
+  console.log('Service started at http://localhost:' + port + '/' + appName + '/');
+};
